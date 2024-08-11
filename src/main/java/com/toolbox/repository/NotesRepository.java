07d549a54dd18f9d567cc2d4263dc8c1438a9ad4
Leslie Gayard <38896383+LesGayard@users.noreply.github.com>
2024-08-11 13:26:00 +0200

    Repositories conflict fixed

--- src/main/java/com/toolbox/repository/NotesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.toolbox.repository;
-
-import com.toolbox.model.Notes;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotesRepository extends JpaRepository<Notes, Integer> {
-}
